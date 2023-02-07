6b1760dc799ebbb630809f39b7c1c1da3e3f4cc7
Donny <donnytkf@gmail.com>
2023-02-07 15:46:29 +0800

    dto ok

--- server/main.ts
+++ server/main.ts
@@ -6,14 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.useGlobalPipes
-    // new ValidationPipe({
-    //   whitelist: true,
-    //   transform: true,
-    //   forbidUnknownValues: false,
-    // }),
-    ();
-
-  await app.listen(3000);
+  app.useGlobalPipes;
+  new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }),
+    await app.listen(3000);
 }
 bootstrap();
